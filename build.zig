9020cbaf0031089945c2fb2c87ec98ea66966761
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-08-12 17:23:26 +0530

    Updated licence

--- build.zig
+++ build.zig
@@ -1,3 +1,19 @@
+// tsundere-runtime build graph: wires the baka SDK, ELF loader, OS shim and runtime into the `tsundere` executable.
+// Copyright (C) 2026  Aakarsh Kashyap
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
--- src/loader/elf_loader.zig
+++ src/loader/elf_loader.zig
@@ -1,3 +1,19 @@
+// elf_loader.zig: ELF header, section and symbol resolution helpers for tsundere-runtime.
+// Copyright (C) 2026  Aakarsh Kashyap
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 const baka = @import("baka");
 const std = @import("std");
 
